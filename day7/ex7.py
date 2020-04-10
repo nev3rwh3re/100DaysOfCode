d1a4692386a958b87df85f88510976f13465c3cf
nev3rwh3re <nev3rwh3re@gmail.com>
2020-04-09 22:13:41 -0400

    Day 7

---
+++ day7/ex7.py
@@ -0,0 +1,38 @@
+# The big print block at the start
+print("Mary had a little lamb.")
+
+# this is kind of interesting. The format and word are put into the place
+# of the curly braces
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+
+# print a line of periods
+print("." * 10) # what'd that do?
+
+# single character variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# print it all out
+# the end=' ' just sets a variable called end to a space and prints it
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# the last of it
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# we are supposed to journal our mistakes. I was lucky and didn't make any 
+# in this one
+
+# other notes - you can use single- or double-quotes to make a string
+# usually use single-quotes for short strings and characters
+
