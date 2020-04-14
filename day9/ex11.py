0456b7389824c493e449c3c11093dde1d84b87ee
nev3rwh3re <nev3rwh3re@gmail.com>
2020-04-13 20:42:25 -0400

    Ex 9-11

---
+++ day9/ex11.py
@@ -0,0 +1,9 @@
+# ex11.py
+print("How old are you?", end=' ')
+age = input()
+print("How tall re you?", end=' ')
+height = input()
+print("how much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
